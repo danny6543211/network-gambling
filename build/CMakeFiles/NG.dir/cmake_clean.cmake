2dcb15d18a4efc6e655859c20d80d68d888f79df
danny6543211 <danny6543211@gmai.com>
2023-10-26 20:56:45 +0800

    coding

---
+++ build/CMakeFiles/NG.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NG.dir/main.cpp.obj"
+  "CMakeFiles/NG.dir/main.cpp.obj.d"
+  "NG.exe"
+  "NG.exe.manifest"
+  "NG.pdb"
+  "libNG.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
